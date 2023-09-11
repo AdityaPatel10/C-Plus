d4da4939d7019b6a0130c9bf3f3c01d5355aca24
Aditya Patel <aditya97556@gmail.com>
2023-09-11 15:23:09 +0530

    Adding Sorting files.

--- DATA STRUCTURE/Sorting/Selection(using function).cpp
+++ DATA STRUCTURE/Sorting/Selection(using function).cpp
@@ -3,25 +3,16 @@ using namespace std;
 int *Selection(int a[], int n){
     int i, loc, j, temp;
     for(i=0 ; i<n-1 ; i++){
-        loc=i;
         for(j=i+1; j<n ; j++){
-            if(a[loc]>a[j]){
-                loc=j;
+            if(a[i]>a[j]){
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
             }
         }
-            if(loc!=i){
-                temp=a[i];
-                a[i]=a[loc];
-                a[loc]=temp;
-            }
     }
     return a;
 }       
-// int *Swap(int a, int b){
-//     int temp=a;
-//     a=b;
-//     b=temp;
-// }
 int Display(int a[], int n){
     for(int i=0 ; i<n ; i++){
         cout<<"Value of a["<<i<<"]= "<<a[i]<<endl;
