b8c8fc4a6147f2cbe806ed702ae35db32146d7e0
Aditya Patel <aditya97556@gmail.com>
2023-11-08 08:39:52 +0530

    Added file

--- DATA STRUCTURE/Arrays/Merged Sorted Array.cpp
+++ DATA STRUCTURE/Arrays/Merged Sorted Array.cpp
@@ -22,6 +22,18 @@ int Sort(int a[], int n){
         }
     }
 }
+void Merge(int a1[], int a2[], int a[], int n1, int n2){
+    int i=0, j=0, k=0;
+    while(i<n1){
+        a[k]= a1[i];
+        i++;k++;
+    }
+    while(j<n2){
+        a[k]= a2[j];
+        j++;k++;
+    }
+    Sort(a, n1+n2);
+}
 int main(){
     int n1,n2;
     cout<<"Enter the size of first array: ";
@@ -33,30 +45,8 @@ int main(){
     Input(a1, n1);
     cout<<"Enter the elements of second array: ";
     Input(a2, n2);
-    int i=0, j=0, k=0;
-    int a[n1+n2];
-    while(i<n1 && j<n2){
-        if(a1[i]<a2[j]){
-            a[k]=a1[i];
-            k++;
-            i++;
-        }
-        else{
-            a[k]= a2[j];
-            k++;
-            j++;
-        }
-    }
-    while(i<n1){
-        a[k]=a1[i];
-        k++;
-        i++;
-    }
-    while(j<n2){
-        a[k]=a2[j];
-        k++;
-        j++;
-    }
+    int a[n1+n2]; 
+    Merge(a1, a2, a, n1, n2);
     Sort(a, n1+n2);
     cout<<"Merged sorted Array is:-"<<endl;
     Display(a, n1+n2);
