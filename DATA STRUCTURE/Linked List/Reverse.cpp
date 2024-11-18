06c6906014915fe3353bf148d44064cb98f02b27
AdityaPatel10 <aditya97556@gmail.com>
2024-11-18 16:10:51 +0530

    Adding DS files

---
+++ DATA STRUCTURE/Linked List/Reverse.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* next;
+    node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+void InsertAtTail(node* &head, int val){
+    node* n = new node(val);
+    if(head == NULL){
+        head = n;
+        return;
+    }
+    node* temp = head;
+    while(temp->next!=NULL){
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+void InsertAtHead(node* &head, int val){
+    node* n = new node(val);
+    n->next = head;
+    head = n;
+}
+void display(node* head){
+    node* temp = head;
+    while(temp != NULL){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+node* reverse(node* head){
+    node* prevptr = NULL;
+    node* currptr = head;
+    node* nextptr;
+    while(currptr != NULL){
+        nextptr = currptr->next;
+        currptr->next = prevptr;
+        prevptr = currptr;
+        currptr = nextptr;
+    }
+    return prevptr;
+}
+int main(){
+    node* head = NULL;
+    InsertAtTail(head,1);
+    InsertAtTail(head,2);
+    InsertAtTail(head,3);
+    InsertAtHead(head,4);
+    InsertAtHead(head,5);
+    display(head);
+    node* NewHead = reverse(head);
+    display(NewHead);
+}
